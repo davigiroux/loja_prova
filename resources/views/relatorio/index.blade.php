a0f1f1de8a56d94c08bd12160deee6fe8660360a
davigiroux <daavigiroux@gmail.com>
2018-05-30 23:19:18 -0400

    mudando título;

--- resources/views/relatorio/index.blade.php
+++ resources/views/relatorio/index.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Loja</title>
 
 
     <!-- Fonts -->
